9971d235274b3a0e8cf5faea5c5883519f7e23d0
sirisangja <51770751+sirisangja@users.noreply.github.com>
2019-10-02 11:28:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def callback():
     user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
     userText = decoded['queryResult']['intent']['displayName']
     sendText(user,userText)
-    if (userText== 'สวัสดี') :
+    if (userText== 'ใคร') :
         sendText('ไอ้บ้า')
     elif (userText == 'หิวไหม') :
         sendText('สวยนะ')
